9c0fd6b1f90f34996b9fe476e08c3287e6aba96f
mel-is-mellow <33165295+mel-is-mellow@users.noreply.github.com>
2019-05-09 09:40:40 -0400

    Add files via upload

---
+++ MayasEngineering/javascript1.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+    $.getJSON(url,function(data){
+        var html ="";
+        $.each(data.items,function(i,item)
+        {
+
+            html += "<a href='"+ item.media.m + "' data-lightbox=newbuilding ";
+            html+= 'data-title="' + item.title + '">';
+            html += "<img src=" + item.media.m + "></a>";
+
+
+
+        });
+        $("#new_building").html(html);
+
+    });
+
+});
